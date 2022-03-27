ee6924b7f0665e18309a04e5729308840e8563f1
SimonePavone <simonebest56@gmail.com>
2022-03-27 12:51:24 +0200

    Manca solo destrutturizzare

---
+++ snack 2/asset/main.js
@@ -0,0 +1,58 @@
+/*Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, 
+falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e
+ stampiamo tutto in console.*/
+
+
+
+ const squadreCalcio =
+[
+    {
+        'nome': 'F.C Internazionale 1908',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        'nome': 'Milan',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        'nome': 'Napoli',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        'nome': 'Lazio',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        'nome': 'Roma',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    }
+];
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+/*let puntieFalli = getRandomInt(100)
+console.log(puntieFalli)*/
+
+for (let  i = 0;  i < squadreCalcio.length;  i++) {
+    let subiti = getRandomInt(100);
+    squadreCalcio[i].puntifatti  = subiti;
+    squadreCalcio[i].fallisubiti  = subiti;
+    //Destrutturizziamo (secondo metodo)
+}
+
+console.log(squadreCalcio);
+
